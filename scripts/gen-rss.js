275610d86ee68706dc37403ab7984ff19b019d91
Alabhya Jindal <alabhya10@gmail.com>
2022-05-04 00:40:41 +0530

    Test push to check new blog page

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Alabhya Jindal',
+    site_url: 'https://alabhyajindal.com',
+    feed_url: 'https://alabhyajindal.com/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
